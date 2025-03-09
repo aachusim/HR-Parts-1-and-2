7417197805253648f23a7edb03ebbe54bf4e61f2
aachusim625 <aachusim625@unm.edu>
2025-03-09 17:54:04 -0600

    Part 2 start

---
+++ PersonImperialSet.java
@@ -0,0 +1,8 @@
+class PersonImperialSet extends PersonSet
+{
+    public void add(Person p){}
+
+    private double convertHeight(double height){return height;}
+
+    private double convertWeight(double weight){return weight;}
+}
---
+++ PersonOrderedSet.java
@@ -0,0 +1,6 @@
+class PersonOrderedSet extends PersonSet
+{
+    public void add(Person p){}
+
+    private void sort(){}
+}
